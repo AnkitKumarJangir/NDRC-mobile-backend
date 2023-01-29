bcc01f9bd0289c9ac44ec88414ad704fbb405f01
AnkitKumarJangir <ankitjangra269@gmail.com>
2023-01-29 16:54:07 +0530

    user

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,7 +29,7 @@ const loginUser = (req, res, next) => {
         expiresIn: 86400, // 24 hours
       });
       res.status(200).send({
-        ...user,
+        data: user,
         token: token,
       });
     });
