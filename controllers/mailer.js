a711f1072fdbe894116e787db9877f5f6e79aba5
AnkitKumarJangir <ankitjangra269@gmail.com>
2023-01-31 14:04:57 +0530

    mailer

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -2,6 +2,9 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_E_PASS,
