d35acb75df448b204d327586ec9ab58bd116e472
AnkitKumarJangir <ankitjangra269@gmail.com>
2023-02-01 21:02:00 +0530

    sec

--- index.js
+++ index.js
@@ -30,7 +30,23 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.AUTH_EMAIL,
+    pass: process.env.AUTH_E_PASS,
+  },
+  replyTo: "noreply.ndrctrans@gmail.com",
+});
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
 // set port, listen for requests
 connectDB().then(() => {
   app.listen(PORT, () => {
