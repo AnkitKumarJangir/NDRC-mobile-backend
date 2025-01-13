fa88a9dbd21ca8164de0e6e8ef68f8976022c3b5
= <=>
2025-01-13 20:30:24 +0530

    change port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./src/app");
 const db = require("./src/db");
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 db.connectDB()
   .then(() => {
